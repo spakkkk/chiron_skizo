dcd72f3305d7baf219b0e6833c5e06949fd4ac17
friedrich420 <kelpidorou@yahoo.com>
2015-01-16 11:09:17 +0200

    sched/fair: Reduce latency
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -38,7 +38,7 @@
 #include "walt.h"
 /*
  * Targeted preemption latency for CPU-bound tasks:
- * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 5ms * (1 + ilog(ncpus)), units: nanoseconds)
  *
  * NOTE: this latency value is not the same as the concept of
  * 'timeslice length' - timeslices in CFS are of variable length
@@ -48,8 +48,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 6000000ULL;
-unsigned int normalized_sysctl_sched_latency = 6000000ULL;
+unsigned int sysctl_sched_latency = 5000000ULL;
+unsigned int normalized_sysctl_sched_latency = 5000000ULL;
 
 unsigned int sysctl_sched_sync_hint_enable = 1;
 unsigned int sysctl_sched_cstate_aware = 1;
