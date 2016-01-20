6de86f53970791990f6abe2f170e4a4df4d2f2e8
Bongkyu Kim <bongkyu.kim@lge.com>
2016-01-20 15:01:08 -0800

    UPSTREAM: lz4: fix wrong compress buffer size for 64-bits
    The current lz4 compress buffer is 16kb on 32-bits, 32kb on 64-bits
    system.  But, lz4 needs only 16kb on both.  On 64-bits, this causes
    wasted cpu cycles for additional memset during every compression.
    
    In case of lz4hc, the current buffer size is (256kb + 8) on 32-bits,
    (512kb + 16) on 64-bits.  But, lz4hc needs only (256kb + 2 * pointer) on
    both.
    
    This patch fixes these wrong compress buffer sizes for 64-bits.
    
    Change-Id: I198c6580aea0985beeb514872babb4c917027c6b
    Signed-off-by: Bongkyu Kim <bongkyu.kim@lge.com>
    Cc: Chanho Min <chanho.min@lge.com>
    Cc: Yann Collet <yann.collet.73@gmail.com>
    Cc: Kyungsik Lee <kyungsik.lee@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -37,6 +37,8 @@
  *	- LZ4 homepage : http://www.lz4.org
  *	- LZ4 source repository : https://github.com/lz4/lz4
  */
+#define LZ4_MEM_COMPRESS	(16384)
+#define LZ4HC_MEM_COMPRESS	(262144 + (2 * sizeof(unsigned char *)))
 
 #ifndef __LZ4_H__
 #define __LZ4_H__
