51e5699d2ffe8712bd1245b0bb8e8ca6546db3d3
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2017-02-22 15:46:31 -0800

    mm: drop zap_details::ignore_dirty
    The only user of ignore_dirty is oom-reaper.  But it doesn't really use
    it.
    
    ignore_dirty only has effect on file pages mapped with dirty pte.  But
    oom-repear skips shared VMAs, so there's no way we can dirty file pte in
    them.
    
    Change-Id: Ife5733540fd14e5276a275d91fb46ee490b6aee0
    Link: http://lkml.kernel.org/r/20170118122429.43661-1-kirill.shutemov@linux.intel.com
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-Commit: da162e9368990ed747075e2ab427da0759fc4a59
    Git-Repo: git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1149,12 +1149,6 @@ again:
 				rss[MM_ANONPAGES]--;
 			else {
 				if (pte_dirty(ptent)) {
-					/*
-					 * oom_reaper cannot tear down dirty
-					 * pages
-					 */
-					if (unlikely(details && details->ignore_dirty))
-						continue;
 					force_flush = 1;
 					set_page_dirty(page);
 				}
