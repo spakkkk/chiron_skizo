332a3399fec1b13c5a2c2ccc7b85f152a680f682
Frederic Weisbecker <fweisbec@gmail.com>
2017-06-29 19:15:09 +0200

    sched/cputime: Rename vtime fields
    The current "snapshot" based naming on vtime fields suggests we record
    some past event but that's a low level picture of their actual purpose
    which comes out blurry. The real point of these fields is to run a basic
    state machine that tracks down cputime entry while switching between
    contexts.
    
    So lets reflect that with more meaningful names.
    
    Tested-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Rik van Riel <riel@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Wanpeng Li <kernellwp@gmail.com>
    Link: http://lkml.kernel.org/r/1498756511-11714-4-git-send-email-fweisbec@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1811,7 +1811,7 @@ struct task_struct {
 	struct prev_cputime prev_cputime;
 #ifdef CONFIG_VIRT_CPU_ACCOUNTING_GEN
 	seqcount_t vtime_seqcount;
-	unsigned long long vtime_snap;
+	unsigned long long vtime_starttime;
 	enum {
 		/* Task is sleeping or running in a CPU with VTIME inactive */
 		VTIME_INACTIVE = 0,
@@ -1819,7 +1819,7 @@ struct task_struct {
 		VTIME_USER,
 		/* Task runs in kernelspace in a CPU with VTIME active */
 		VTIME_SYS,
-	} vtime_snap_whence;
+	} vtime_state;
 #endif
 	unsigned long nvcsw, nivcsw; /* context switch counts */
 	u64 start_time;		/* monotonic time in nsec */
