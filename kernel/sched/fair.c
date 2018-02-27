af639e7ebec4ca17109df4f769a3c44259fb78ed
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-02-27 15:41:21 +0530

    sched/fair: Remove EAS enforcement in find_best_target()
    commit 63c275027866 ("sched: fair: Always try to use energy
    efficient cpu for wakeups") forces energy aware task placement
    for all tasks. So there is no need to have any additional
    checks in find_best_target() to enforce EAS mode.
    
    Change-Id: I001326afcd14b221dbbc545b82d4f9204bf7417d
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6804,19 +6804,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			}
 
 			/*
-			 * Enforce EAS mode
-			 *
-			 * For non latency sensitive tasks, skip CPUs that
-			 * will be overutilized by moving the task there.
-			 *
-			 * The goal here is to remain in EAS mode as long as
-			 * possible at least for !prefer_idle tasks.
-			 */
-			if ((new_util * capacity_margin) >
-			    (capacity_orig * SCHED_CAPACITY_SCALE))
-				continue;
-
-			/*
 			 * Skip processing placement further if we are visiting
 			 * cpus with lower capacity than start cpu
 			 */
