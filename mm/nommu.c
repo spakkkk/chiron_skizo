f9c2a293779275cfa965393169368b81f85839af
Laurent Dufour <ldufour@linux.vnet.ibm.com>
2018-04-17 16:33:13 +0200

    mm: introduce INIT_VMA()
    Some VMA struct fields need to be initialized once the VMA structure is
    allocated.
    Currently this only concerns anon_vma_chain field but some other will be
    added to support the speculative page fault.
    
    Instead of spreading the initialization calls all over the code, let's
    introduce a dedicated inline function.
    
    Change-Id: I9f6b29dc74055354318b548e2b6b22c37d4c61bb
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    Patch-mainline: linux-mm @ Tue, 17 Apr 2018 16:33:13
    [vinmenon@codeaurora.org: 4.9 porting changes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- mm/nommu.c
+++ mm/nommu.c
@@ -1275,7 +1275,7 @@ unsigned long do_mmap(struct file *file,
 	region->vm_flags = vm_flags;
 	region->vm_pgoff = pgoff;
 
-	INIT_LIST_HEAD(&vma->anon_vma_chain);
+	INIT_VMA(vma);
 	vma->vm_flags = vm_flags;
 	vma->vm_pgoff = pgoff;
 
