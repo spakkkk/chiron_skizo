c3a6c1f1dbda324ed4f1ca399d0aa24f81f45399
Angelo G. Del Regno <kholk11@gmail.com>
2019-02-05 16:18:26 +0100

    power: smbcharger: Fix missing function usage
    The is_bms_psy_present function is missing from our version of
    the smbcharger driver.
    Replace the function calls with simple checks.
    
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -3958,7 +3958,7 @@ static void smbchg_chg_led_brightness_set(struct led_classdev *cdev,
 	u8 reg;
 	int rc;
 
-	if (!is_bms_psy_present(chip)) {
+	if (!chip->bms_psy) {
 		dev_err(chip->dev, "Couldn't access bms psy\n");
 		return;
 	}
@@ -4010,7 +4010,7 @@ static void smbchg_chg_led_blink_set(struct smbchg_chip *chip,
 	u8 reg;
 	int rc;
 
-	if (!is_bms_psy_present(chip)) {
+	if (!chip->bms_psy) {
 		dev_err(chip->dev, "Couldn't access bms psy\n");
 		return;
 	}
