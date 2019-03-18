32a7ec427373a1f1e7654f09f80e543c414f5db3
Danny Lin <danny@kdrag0n.dev>
2019-03-17 19:34:57 -0700

    mm: process_reclaim: Bump maximum pages per reclaim attempt
    4 MiB isn't a meaningful amount to reclaim. Bump the limit to 32 MiB
    instead.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- mm/process_reclaim.c
+++ mm/process_reclaim.c
@@ -35,7 +35,7 @@ module_param_named(enable_process_reclaim, enable_process_reclaim, int,
 	S_IRUGO | S_IWUSR);
 
 /* The max number of pages tried to be reclaimed in a single run */
-int per_swap_size = SWAP_CLUSTER_MAX * 32;
+int per_swap_size = SWAP_CLUSTER_MAX * 256;
 module_param_named(per_swap_size, per_swap_size, int, S_IRUGO | S_IWUSR);
 
 int reclaim_avg_efficiency;
