00a982c47d4c9363acac5b4d9c9b5556305ecea3
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-30 20:40:59 -0700

    ion: Overhaul for vastly improved clarity and performance
    The ION driver suffers from massive code bloat caused by excessive
    debug features, as well as poor lock usage as a result of that. Multiple
    locks in ION exist to make the debug features thread-safe, which hurts
    ION's actual performance when doing its job.
    
    There are numerous code paths in ION that hold mutexes for no reason and
    hold them for longer than necessary. This results in not only unwanted
    lock contention, but also long delays when a mutex lock results in the
    calling thread getting preempted for a while. All lock usage in ION
    follows this pattern, which causes poor performance across the board.
    Furthermore, a single big lock is used mostly everywhere rather than
    multiple fine-grained locks.
    
    Most of the mutex locks can be replaced with simple atomic operations.
    Where a mutex lock can't be eliminated completely, a spinlock or rwlock
    can be used instead for quick operations, thereby avoiding long delays
    due to preemption. Fine-grained locks are also now used in place of the
    single big lock that was used before.
    
    Additionally, ion_dupe_sg_table is called very frequently, and lies
    within the rendering path for the display. Speed it up by reserving
    caches for its sg_table and page-sized scatterlist allocations, as well
    as by improving the sg copy process. Note that sg_alloc_table zeroes
    out `table`, so there's no need to zero it out using the memory
    allocator.
    
    Overall, just rewrite ION entirely to remedy its sorry situation. This
    optimizes ION for excellent performance and discards its rarely-used
    debug bloat.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -70,7 +70,7 @@ static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 
 static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 {
-	mutex_lock(&pool->mutex);
+        spin_lock(&pool->lock);
 	page->private = ION_PAGE_CACHE;
 	if (PageHighMem(page)) {
 		list_add_tail(&page->lru, &pool->high_items);
@@ -82,7 +82,7 @@ static int ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 
 	if (pool->inode && pool->order == 0)
 		__SetPageMovable(page, pool->inode->i_mapping);
-	mutex_unlock(&pool->mutex);
+	spin_unlock(&pool->lock);
 	return 0;
 }
 
@@ -114,12 +114,12 @@ void *ion_page_pool_alloc(struct ion_page_pool *pool, bool *from_pool)
 
 	*from_pool = true;
 
-	if (mutex_trylock(&pool->mutex)) {
+	if (spin_trylock(&pool->lock)) {
 		if (pool->high_count)
 			page = ion_page_pool_remove(pool, true);
 		else if (pool->low_count)
 			page = ion_page_pool_remove(pool, false);
-		mutex_unlock(&pool->mutex);
+		spin_unlock(&pool->lock);
 	}
 	if (!page) {
 		page = ion_page_pool_alloc_pages(pool);
@@ -141,12 +141,12 @@ void *ion_page_pool_alloc_pool_only(struct ion_page_pool *pool)
 
 	BUG_ON(!pool);
 
-	if (mutex_trylock(&pool->mutex)) {
+	if (spin_trylock(&pool->lock)) {
 		if (pool->high_count)
 			page = ion_page_pool_remove(pool, true);
 		else if (pool->low_count)
 			page = ion_page_pool_remove(pool, false);
-		mutex_unlock(&pool->mutex);
+		spin_unlock(&pool->lock);
 	}
 	if (page) {
 		lock_page(page);
@@ -197,16 +197,16 @@ int ion_page_pool_shrink(struct ion_page_pool *pool, gfp_t gfp_mask,
 	while (freed < nr_to_scan) {
 		struct page *page;
 
-		mutex_lock(&pool->mutex);
+		spin_lock(&pool->lock);
 		if (pool->low_count) {
 			page = ion_page_pool_remove(pool, false);
 		} else if (high && pool->high_count) {
 			page = ion_page_pool_remove(pool, true);
 		} else {
-			mutex_unlock(&pool->mutex);
+			spin_unlock(&pool->lock);
 			break;
 		}
-		mutex_unlock(&pool->mutex);
+		spin_unlock(&pool->lock);
 		ion_page_pool_free_pages(pool, page);
 		freed += (1 << pool->order);
 	}
@@ -225,15 +225,15 @@ static bool ion_page_pool_isolate(struct page *page, isolate_mode_t mode)
 		return false;
 	pool = mapping->private_data;
 
-	mutex_lock(&pool->mutex);
+	spin_lock(&pool->lock);
 	/* could be removed from the cache pool and thus become unmovable */
 	if (!__PageMovable(page)) {
-		mutex_unlock(&pool->mutex);
+		spin_unlock(&pool->lock);
 		return false;
 	}
 
 	if (unlikely(!test_bit(ION_PAGE_CACHE, &page->private))) {
-		mutex_unlock(&pool->mutex);
+		spin_unlock(&pool->lock);
 		return false;
 	}
 
@@ -242,7 +242,7 @@ static bool ion_page_pool_isolate(struct page *page, isolate_mode_t mode)
 		pool->high_count--;
 	else
 		pool->low_count--;
-	mutex_unlock(&pool->mutex);
+	spin_unlock(&pool->lock);
 
 	return true;
 }
@@ -263,7 +263,7 @@ static int ion_page_pool_migrate(struct address_space *mapping,
 	__ClearPageMovable(page);
 	ClearPagePrivate(page);
 	unlock_page(page);
-	mutex_lock(&pool->mutex);
+	spin_lock(&pool->lock);
 	if (PageHighMem(newpage)) {
 		list_add_tail(&newpage->lru, &pool->high_items);
 		pool->high_count++;
@@ -271,7 +271,7 @@ static int ion_page_pool_migrate(struct address_space *mapping,
 		list_add_tail(&newpage->lru, &pool->low_items);
 		pool->low_count++;
 	}
-	mutex_unlock(&pool->mutex);
+	spin_unlock(&pool->lock);
 	put_page(page);
 	return 0;
 }
@@ -364,7 +364,7 @@ struct ion_page_pool *ion_page_pool_create(struct device *dev, gfp_t gfp_mask,
 	INIT_LIST_HEAD(&pool->high_items);
 	pool->gfp_mask = gfp_mask;
 	pool->order = order;
-	mutex_init(&pool->mutex);
+	spin_lock_init(&pool->lock);
 	plist_node_init(&pool->list, order);
 
 	pool->inode = NULL;
